aebce1c682373baa8be41c73b86f5c0ad1ef0d2d
Neikho <albathomas62250@gmail.com>
2016-11-17 21:45:48 +0100

    Ajout date crea + modif, optimisation des requetes

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 ?>
 
 <?php
-    if(isset($_GET['id']) && isset($_POST['message'])){
-        $queryupdate = $pdo->prepare("UPDATE messages SET contenu=(:contenuup) WHERE id=:amaj");
+    $current = time();
+    if(isset($_GET['id']) && isset($_POST['message']))
+    {
+        $queryupdate = $pdo->prepare("UPDATE messages SET contenu=(:contenuup), creation=(:modif) WHERE id=:amaj");
         $queryupdate->bindValue(':amaj', $_GET['id']);
+        $queryupdate->bindValue(':modif', $current);
         $queryupdate->bindValue(':contenuup', $_POST['message']);
         $queryupdate->execute();
         header('Location: http://localhost/micro_blog/index.php');
@@ -14,7 +17,8 @@
 ?>
 
 <?php
-    if(isset($_GET['idsupp']) && !empty($_GET['idsupp'])){
+    if(isset($_GET['idsupp']) && !empty($_GET['idsupp']))
+    {
         $querysupp = $pdo->prepare("DELETE FROM messages WHERE id=:asupp");
         $querysupp->bindValue(':asupp', $_GET['idsupp']);
         $querysupp->execute();
@@ -28,26 +32,26 @@
                <?php
                     if (isset($_GET['id']) && !empty($_GET['id'])) 
                     {
-                        $requete = $pdo->prepare("SELECT contenu FROM messages WHERE id=:contenuZ");
-                        $requete->bindValue(':contenuZ', $_GET['id']);
-                        $requete->execute();
-                        $data=$requete->fetch();
+                        $selectContenu = $pdo->prepare("SELECT contenu FROM messages WHERE id=:contenuZ");
+                        $selectContenu->bindValue(':contenuZ', $_GET['id']);
+                        $selectContenu->execute();
+                        $data=$selectContenu->fetch();
                     }   
                 ?>
-                <textarea id="message" name="message" class="form-control" placeholder="Message"><?php if (isset($requete)){echo $data['contenu'];} ?></textarea>
+                <textarea id="message" name="message" class="form-control" placeholder="Message"><?php if (isset($selectContenu)){echo $data['contenu'];} ?></textarea>
             </div>
         </div>
         <div class="col-sm-2">
             <button type="submit" class="btn btn-success btn-lg">
             <?php 
-            if(!isset($_GET['id']))
-            {
-                echo 'Envoyer';
-            }
-            else if(isset($_GET['id']))
-            {
-                   echo 'Modifier';
-            } 
+                if(!isset($_GET['id']))
+                {
+                    echo 'Envoyer';
+                }
+                else if(isset($_GET['id']))
+                {
+                       echo 'Modifier';
+                } 
             ?>
             </button>
         </div>                        
@@ -55,16 +59,26 @@
 </div>
 
 <?php
-
-    $query = 'SELECT * FROM messages';
-    $stmt = $pdo->query($query);
-    while ($data = $stmt->fetch()) {
+    $selectAll = $pdo->query('SELECT * FROM messages');
+    while ($data = $selectAll->fetch()) 
+    {
 ?>
 <blockquote>
     <input type="hidden" name="id" value="<?php $data['id'] ?>">
 	<?= $data['contenu'] ?>
-     <a href="index.php?id=<?php echo $data['id']; ?>"><button class="btn btn-warning btn-xs">Modifier</button></a>
-     <a href="index.php?idsupp=<?php echo $data['id']; ?>"><button class="btn btn-danger btn-xs">Supprimer</button></a>
+    <span class="infostemps">
+    <br/>
+    <?php 
+        $crea = $data['creation'];
+        echo "Crée le : ".date('d/m/Y', $crea); 
+    ?> <br/>
+    <?php 
+        echo " Modifié le : ".date('H:i:s', $crea); 
+    ?>
+    </span>
+    <a href="index.php?idsupp=<?php echo $data['id']; ?>" class="bout"><button class="btn btn-danger btn-sm">Supprimer</button></a>
+    <a href="index.php?id=<?php echo $data['id']; ?>" class="bout"><button class="btn btn-warning btn-sm">Modifier</button></a>
+     
 </blockquote>
 <?php
     }
--- messages.php
+++ messages.php
@@ -3,10 +3,13 @@
 ?>
 
 <?php
-	if (isset($_POST['message']) && !empty($_POST['message'])) {
-	    $query = 'INSERT INTO messages (contenu) VALUES (:contenu)';
+	$current = time();
+	if (isset($_POST['message']) && !empty($_POST['message'])) 
+	{
+	    $query = 'INSERT INTO messages (contenu,creation) VALUES (:contenu,:crea)';
 	    $prep = $pdo->prepare($query);
 	    $prep->bindValue(':contenu', $_POST['message']);
+	    $prep->bindValue(':crea', $current);
 	    $prep->execute();
 	    header('Location: http://localhost/micro_blog/index.php');
 	}
